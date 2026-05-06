99a50c3490a6d3720cb394acd7911081abf73ea6
Anshuman Varma <anshumanverma9795@gmail.com>
2026-05-06 15:59:26 +0530

    feat: update root route response message

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Radhe Radhe 😊');
+  res.send('Every day is a new opportunity to start fresh 😊');
 });
 
 app.listen(port, () => {
